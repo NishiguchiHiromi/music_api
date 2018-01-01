53e7aeb06b37fbf9e3869ebf370cfc656de94d24
vagrant <vagrant@localhost.localdomain>
2018-01-01 13:52:23 +0000

    deploy

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module TestApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
 
       allow do
         origins '*'
